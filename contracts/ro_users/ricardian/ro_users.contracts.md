c28d46893ff672f6b969cf6d1bd43642a9f801b2
Bradley Hart <bradley.hart-ext@block.one>
2021-07-20 15:21:08 -0400

    additional changes

--- contracts/ro_users/ricardian/ro_users.contracts.md
+++ contracts/ro_users/ricardian/ro_users.contracts.md
@@ -1,11 +1,7 @@
-<h1 class="contract"> getbyaccname </h1>
+<h1 class="contract"> get </h1>
 
-Given an `account_name`, it will return back the associated entry in the TODO database.
+Retrieves data from two tables of user data and returns the combined result of user objects
 
-<h1 class="contract"> upsert </h1>
+<h1 class="contract"> put </h1>
 
-Given a `uuid`, `account_name`, a `task`, and a `checked` predicated. This action will either insert a new record with the associated fields or update a preexisting one.
-
-<h1 class="contract"> del </h1>
-
-Given a `uuid` this action will delete the record associated to it.
+Given an `id`, `name`, `state`, and `age`, this action will insert a new record with the associated fields.
--- src/index.tsx
+++ src/index.tsx
@@ -18,18 +18,18 @@ import actions from './actions'
 const rpc = new JsonRpc('')
 const privateKey = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
 
-export interface Entry {
-  uuid: string,
-  account_name: string,
-  task: string,
-  checked: boolean,
-  created: number,
+export interface User {
+  id: number,
+  name: string,
+  state: string,
+  age: number|'',
 }
 
 const App: React.FC = () => {
   const [api] = useState<Api>(new Api({ rpc, signatureProvider: new JsSignatureProvider([privateKey]) }))
   const [error, setError] = useState<string>('')
   const [list, setList] = useState<any>()
+  const [nextId, setNextId] = useState<number>(0)
 
   useEffect(() => {
     getList() // eslint-disable-line @typescript-eslint/no-floating-promises
@@ -40,52 +40,48 @@ const App: React.FC = () => {
     if (api === undefined) return setError('Unexpected error: Api object is not set.')
 
     try {
-      const transactionResult = await actions.getbyaccname(api)
-      const entries = transactionResult.processed.action_traces[0].return_value_data.map((entry: any) => {
-        entry.checked = entry.checked === 1
-        return entry
-      }).sort((a: Entry, b: Entry) => {
-        if (a.created < b.created) return -1
-        if (a.created > b.created) return 1
-        return 0
-      })
+      const transactionResult = await actions.get(api)
+      const entries = transactionResult.result.action_traces[0].return_value_data
+        .sort((a: User, b: User) => a.id - b.id)
+      if (entries.length !== 0) setNextId(entries[entries.length-1].id+1)
       setList(entries)
     } catch (e) {
       setError(e.message)
     }
   }
 
-  const upsert = async (entry: Entry) => {
+  const put = async (user: User) => {
     setError('')
     if (api === undefined) return setError('Unexpected error: Api object is not set.')
 
     try {
-      await actions.upsert(api, entry)
+      await actions.put(api, user)
       await getList()
     } catch (e) {
       setError(e.message)
     }
   }
 
-  const del = async (uuid: string) => {
-    setError('')
-    if (api === undefined) return setError('Unexpected error: Api object is not set.')
-
-    try {
-      await actions.del(api, uuid)
-      setList(list.filter((entry: Entry) => entry.uuid !== uuid))
-    } catch (e) {
-      setError(e.message)
-    }
-  }
-
   return (
-    <div className='ui text container'>
-      <h1 className='ui header centered'>Key Value Example App</h1>
-      <NewEntry upsert={upsert} />
-      <div id="list" className="ui raised segments">
-        {list && list.map((entry: Entry) => <EntryComponent entry={entry} key={entry.uuid} upsert={upsert} del={del} />)}
-        {(!list || list.length === 0) && <div className="ui segment center aligned">No Tasks To Display, Create A New Task!</div>}
+    <div className='ui container'>
+      <h1 className='ui header centered'>Read Only Example App</h1>
+      <div className='ui fluid container right aligned'>
+        <NewUserModal nextId={nextId} put={put} />
+      </div>
+      <div id="list" className="ui raised segment center aligned">
+        {list && <table className="ui celled table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>State</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((user: User) => <UserComponent key={user.id} user={user} />)}
+          </tbody>
+        </table>}
+        {(!list || list.length === 0) && <div>No Users To Display, Create A New User!</div>}
       </div>
       {error && <Error error={error} />}
     </div>
